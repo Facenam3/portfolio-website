8af42eb321a5494b40ef9ad0a8cae157cd83a3b3
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-09-14 23:31:25 +0200

    added scrolls

--- portfolio-website/js/main.js
+++ portfolio-website/js/main.js
@@ -45,6 +45,31 @@ function lightMode() {
     localStorage.setItem("mode", "light");
 }
 
+const scrollHeader = () => {
+    const navbar = document.getElementById("navbar");
+    const aTag = document.querySelectorAll("nav ul li a");
+    const themeToggle = document.getElementById("theme-toggle");
+    const hamburger = document.getElementById("hamburger");
+    const logo = document.getElementById("logo");
+
+    if(window.scrollY >= 200) {
+        navbar.classList.add("bg-primaryColor");
+        aTag.forEach((item) => {
+            item.classList.add("text-whiteColor");
+        });
+        themeToggle.classList.add("text-whiteColor");
+        hamburger.classList.add("text-whiteColor");
+    } else {
+        navbar.classList.remove("bg-primaryColor");
+         aTag.forEach((item) => {
+            item.classList.remove("text-whiteColor");
+        });
+        themeToggle.classList.remove("text-whiteColor");
+        hamburger.classList.add("text-whiteColor");
+    }
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 const scrollUp = () => {
     const scrollUpBtn = document.getElementById("scroll-up");
@@ -56,6 +81,63 @@ const scrollUp = () => {
         scrollUpBtn.classList.add("-bottom-1/2");
         scrollUpBtn.classList.remove("bottom-4");
     }
-}
+};
 
 window.addEventListener("scroll" , scrollUp);
+
+const activeLink = () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    let current = "hero";
+
+    sections.forEach((section) => {
+        const sectioTop = section.offsetTop;
+
+        if(this.scrollY >= sectioTop - 60) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(item => {
+        item.classList.remove("active");
+        if(item.href.includes(current)) {
+            item.classList.add("active");
+        }
+    });
+};
+
+window.addEventListener("scroll", activeLink);
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400
+});
+
+sr.reveal(".hero__image");
+sr.reveal(".hero__content" , {origin: "bottom"});
+sr.reveal(".hero__footer" , {origin: "bottom", delay: 1200});
+
+sr.reveal(".service__top" , {origin: "bottom"});
+sr.reveal(".service_item" , {origin: "bottom", interval: 300});
+
+sr.reveal(".recent_works_top" , {origin: "bottom"});
+sr.reveal(".recent_works_tabs" , {origin: "bottom", delay: 800});
+sr.reveal(".work_card" , {origin: "bottom", delay: 1200});
+
+sr.reveal(".exp__top" , {origin: "top"});
+sr.reveal(".exp_card" , {origin: "right", interval: 300});
+
+sr.reveal(".edu__top" , {origin: "top"});
+sr.reveal(".edu_card" , {origin: "left", interval: 300});
+
+sr.reveal(".skills_top" , {origin: "bottom"});
+sr.reveal(".skills_card" , {origin: "bottom", interval: 300});
+
+sr.reveal(".project_top" , {origin: "top"});
+sr.reveal(".blog_card" , {origin: "top", interval: 300});
+
+sr.reveal(".contact_form" , {origin: "left"});
+sr.reveal(".contact_item" , {origin: "right", interval: 300});
