72c359aefb759afd9895cc6214a8b47a30487c44
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-09-14 22:16:49 +0200

    added scroll

--- portfolio-website/js/main.js
+++ portfolio-website/js/main.js
@@ -45,3 +45,17 @@ function lightMode() {
     localStorage.setItem("mode", "light");
 }
 
+
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+
+    if(this.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-4");
+    }
+}
+
+window.addEventListener("scroll" , scrollUp);
